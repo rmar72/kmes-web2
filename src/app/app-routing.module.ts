2167abb8a02bb7621ae632d6caeb2238669a10ad
jonathan <jmuniz@bbgres.com>
2019-12-11 15:41:13 -0700

    basic nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
+import { IdentityManagementComponent } from './identity-management/identity-management.component';
+import { SystemAdminComponent } from './system-admin/system-admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'identity-management', component: IdentityManagementComponent },
+  { path: 'system-admin', component: SystemAdminComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: DashboardComponent }
 ];
 
 @NgModule({
