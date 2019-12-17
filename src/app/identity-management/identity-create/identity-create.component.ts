69317a4239e356ce550413aa7c192ae4da6bbd2c
Anna Casper <annacasper@spireadmins-MacBook-Pro.local>
2019-12-17 14:19:53 -0700

    FUTX-11 - move create identity form into new component

---
+++ src/app/identity-management/identity-create/identity-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-identity-create',
+  templateUrl: './identity-create.component.html',
+  styleUrls: ['./identity-create.component.scss']
+})
+export class IdentityCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
