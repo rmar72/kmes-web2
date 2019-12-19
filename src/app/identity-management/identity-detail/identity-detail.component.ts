06807125dc45a09547b6e0b06606ede2f3fe99f4
rmar72 <rsolm72@gmail.com>
2019-12-19 14:48:53 -0700

    new identity detail component with demo boilerplate

---
+++ src/app/identity-management/identity-detail/identity-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-identity-detail',
+  templateUrl: './identity-detail.component.html',
+  styleUrls: ['./identity-detail.component.scss']
+})
+export class IdentityDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  
+  }
+
+}
