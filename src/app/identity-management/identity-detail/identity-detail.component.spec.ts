393bc51d07e0fef2708772492571bf94e8cc95da
rmar72 <rsolm72@gmail.com>
2019-12-30 15:20:20 -0700

    FUTX-17 passing tests

--- src/app/identity-management/identity-detail/identity-detail.component.spec.ts
+++ src/app/identity-management/identity-detail/identity-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { InitialsPipe } from 'src/app/shared/pipes/initials.pipe';
 import { IdentityDetailComponent } from './identity-detail.component';
 
 describe('IdentityDetailComponent', () => {
@@ -8,7 +8,7 @@ describe('IdentityDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IdentityDetailComponent ]
+      declarations: [ IdentityDetailComponent, InitialsPipe ]
     })
     .compileComponents();
   }));
