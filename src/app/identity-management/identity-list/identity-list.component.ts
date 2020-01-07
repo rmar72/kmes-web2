d9dc80130a93cadb9797f873c4616d311301abbb
rmar72 <rsolm72@gmail.com>
2020-01-07 00:07:01 -0700

    initial bulk delete operation on identities

--- src/app/identity-management/identity-list/identity-list.component.ts
+++ src/app/identity-management/identity-list/identity-list.component.ts
@@ -39,4 +39,53 @@ export class IdentityListComponent implements OnInit {
       }
     );
   }
+
+  checkedBoxes = {}
+  deleteIdtys = []
+
+  addCheckBox(e, idty){
+    this.checkedBoxes[idty.username] = e.target.checked;
+
+    if(this.checkedBoxes[idty.username]){
+      this.deleteIdtys.push(idty.username);
+    }
+    else {
+      this.deleteIdtys = this.deleteIdtys.filter(idtyName => idtyName !== idty.username);
+    }
+  }
+  
+
+  deleteIdentities(){
+    if(this.deleteIdtys.length > 0){
+      let deleteConfirm = confirm(`You are about to delete ${this.deleteIdtys.length} Identities. This action cannot be undone. Would you like to continue?`);
+
+      if(deleteConfirm){
+          
+        const deleteList = this.deleteIdtys
+          .map(idty => this.usersService.usersDelete(idty).pipe(
+            catchError(err => of(`handled err ${err}`))
+          ));
+        
+        forkJoin(...deleteList).subscribe(
+          val => {
+            // temp solution for demo purposes, with emitter to idty mngt comp. api call will replinish list 
+            if(this.identities.length == 1){
+              this.identityDeleted.emit(this.identities[0].username);
+            }
+
+            this.identities = this.identities.filter(idty =>
+              this.deleteIdtys.indexOf(idty.username) < 0
+            );
+            this.deleteIdtys = [];
+          },
+          err => console.log(err)
+        );
+      }
+    }
+    else {
+      alert("No identities have been selected.")
+    }
+  }
+  
+
 }
