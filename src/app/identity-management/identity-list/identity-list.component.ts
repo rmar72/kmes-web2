f9a7b437ed7c6c688734b3130e6347c260357ddb
rmar72 <rsolm72@gmail.com>
2020-01-08 23:34:09 -0700

    logic for selectAll mastercheckbox

--- src/app/identity-management/identity-list/identity-list.component.ts
+++ src/app/identity-management/identity-list/identity-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter, ɵEMPTY_ARRAY, ElementRef } from '@angular/core';
 import { UsersServiceProxy } from 'src/app/shared/api/service-proxies';
-import { from, of, forkJoin } from "rxjs";
-import { catchError, map, take } from 'rxjs/operators';
+import { of, forkJoin } from "rxjs";
+import { catchError, retry } from 'rxjs/operators';
 import { AppToastService } from 'src/app/shared/services/app-toast.service';
 
 @Component({
@@ -18,8 +18,9 @@ export class IdentityListComponent implements OnInit {
   identityCount: number;
   deleteError = '';
   errorIndex: number;
-  identityIsChecked = {};
+  idtyIsChecked = {};
   deleteIdtys = [];
+  masterCheckbox: boolean = false;
 
   constructor(
     private usersService: UsersServiceProxy,
@@ -49,101 +50,59 @@ export class IdentityListComponent implements OnInit {
     );
   }
 
-  masterCheckbox: boolean = false;
-  onSelectAllBoxes(e){
+  deleteSelectAll(){
     let checkboxes = this.checkbox.nativeElement.querySelectorAll('.idty-checkbox');
-    this.masterCheckbox = !this.masterCheckbox;
+    this.masterCheckbox= !this.masterCheckbox;
 
-    if(!this.masterCheckbox && this.deleteIdtys.length >= 1){
-      checkboxes.forEach(element => {
-        if(element.checked)
-          element.checked = !element.checked;
+    if(this.masterCheckbox){
+      checkboxes.forEach(box => { 
+        setTimeout(() => box.checked = true, 0);
       });
+
+      this.deleteIdtys = [
+        ...this.identities.map(idty => {
+          this.idtyIsChecked[idty.username] = true;
+          return idty.username;
+        })
+      ];
+    }
+    else{
+      checkboxes.forEach(box => {
+        setTimeout(() => box.checked = false, 0);
+      });
+
+      this.identities.forEach( idty => this.idtyIsChecked[idty.username] = false );
       this.deleteIdtys = [];
     }
-    if(this.masterCheckbox === true){
-      checkboxes.forEach(element => {
-        if(element.checked == false)
-          element.checked = !element.checked;
-      });
-
-      from(this.identities).pipe(
-        map(idty => idty.username)
-        // take(2), whatever is available on the page
-      )
-      .subscribe(value => {
-        this.deleteIdtys.push(value);
-      });
-    }
-    else {
-      checkboxes.forEach(element => {
-        element.checked = false;
-        this.deleteIdtys = [];
-      });
-    }
-
   }
 
-  onSelectAllBoxes2(){
-    alert(1)
-  }
 
-  onBoxCheck2(e, checkedIdty){
-    this.identityIsChecked[checkedIdty.username] = e.target.checked;
+  onBoxCheck(e, checkIdty){
+    this.idtyIsChecked[checkIdty.username] = e.target.checked;
 
-    if(this.identityIsChecked[checkedIdty.username]){
-      this.deleteIdtys.push(checkedIdty.username);
-      console.log("this.deleteIdtys", this.deleteIdtys)
+    if(this.idtyIsChecked[checkIdty.username]){
+      this.deleteIdtys.push(checkIdty.username);
     }
     else {
-      this.deleteIdtys = this.deleteIdtys.filter(queuedIdtyName => queuedIdtyName !== checkedIdty.username);
-      console.log("this.deleteIdtys", this.deleteIdtys)
+      this.deleteIdtys = this.deleteIdtys.filter(queuedIdtyName => queuedIdtyName !== checkIdty.username);
     }
-    
+
     this.masterCheckbox = this.deleteIdtys.length == this.identities.length ? true : false;
   }
 
-  onBoxCheck(e, idty){
-    console.log(0)
-    this.identityIsChecked[idty.username] = e.target.checked;
-    if(this.deleteIdtys.length !== this.identities.length){
-      this.masterCheckbox = false;
-    }
-    if(this.masterCheckbox){ // if its checked, and we are executing this fn then obv we've deselected so its no longer all of em
-      console.log(1) 
-      this.masterCheckbox = !this.masterCheckbox;
-    }
-
-    if(this.identityIsChecked[idty.username]){
-      console.log(2)
-      this.deleteIdtys.push(idty.username);
-    }
-    else {
-      console.log(3)
-      this.deleteIdtys = this.deleteIdtys.filter(idtyName => idtyName !== idty.username);
-    }
-
-    if(this.deleteIdtys.length == this.identities.length){ // if all boxes selected toggle master checkbox
-      console.log(4)
-      this.masterCheckbox = !this.masterCheckbox;
-    }
-  }
-
   deleteIdentities(){
-    // console.log("this.deleteIdtys", this.deleteIdtys)
     if(this.deleteIdtys.length > 0){
       let deleteConfirm = confirm(`You are about to delete ${this.deleteIdtys.length} Identities. This action cannot be undone. Would you like to continue?`);
 
       if(deleteConfirm){
-          
         const deleteList = this.deleteIdtys.map(idty => 
           this.usersService.usersDelete(idty).pipe(
+            retry(1),
             catchError(err => of(`handled err ${err}`))
           ));
         
         forkJoin(...deleteList).subscribe(val => {
-            // temp solution for demo purposes
-            // later on, this wont be used since idty mgt will replinish the list
+            // temp solution for demo purposes. later on, idty mgt will replinish/update the list
             if(this.identities.length == 1){
               this.identityDeleted.emit(val);
             }
