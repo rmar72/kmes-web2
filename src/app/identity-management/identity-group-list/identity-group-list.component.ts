933b606cff0b43fe7d9bc88c71c8d24a9c39b7e3
Anna Casper <annacasper@spireadmins-MacBook-Pro.local>
2020-01-15 14:23:44 -0700

    FUTX-22 - add identity list styling

--- src/app/identity-management/identity-group-list/identity-group-list.component.ts
+++ src/app/identity-management/identity-group-list/identity-group-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { UsersServiceProxy } from 'src/app/shared/api/service-proxies';
 
 declare var $;
 
@@ -11,7 +12,8 @@ export class IdentityGroupListComponent implements OnInit {
   @Input() identityGroups: any[];
   @Input() groupCount: string;
 
-  constructor() { }
+  constructor(
+    private usersService: UsersServiceProxy) { }
 
   ngOnInit() {
     $(document).ready(() => {
@@ -23,4 +25,47 @@ export class IdentityGroupListComponent implements OnInit {
     return `${count} identities required for login`;
   }
 
+  getIdentitiesForGroup(groupName: string, i: number, c?: number) {
+    if(this.identityGroups[i].numUsers > 0) {
+      let mockData = [
+        {
+          "username": "User1",
+          "primaryGroup": "Admin",
+          "subGroups": [
+            "Child Group1",
+            "Child Group2"
+          ],
+          "valid": true,
+          "lastLogin": "2019-12-10 23:59:59"
+        },
+        {
+          "username": "User2",
+          "primaryGroup": "Admin",
+          "subGroups": [
+            "Child Group1",
+            "Child Group2"
+          ],
+          "valid": true,
+          "lastLogin": "2019-12-10 23:59:59"
+        }
+      ]
+      if(c && this.identityGroups[i].childGroups[c] && !this.identityGroups[i].childGroups[c].identities) {
+        this.identityGroups[i].childGroups[c].identities = mockData;
+      } else if(!this.identityGroups[i].identities) {
+        this.identityGroups[i].identities = mockData;
+  
+        $(".toggle-popover").popover({ trigger: "hover" });
+  
+        // TO DO use actual service call when API is working 
+        // this.usersService.usersGet(undefined, groupName, undefined, undefined).subscribe(
+        //   (response) => {
+        //     this.identityGroups[i].identities = response.responseData.users;
+        //   },
+        //   (error) => {
+        //     console.log(error)
+        //   }
+        // );
+      }
+    }
+  }
 }
